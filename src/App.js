12f111a53fa0fbc14b15a3ec05cd39133bf975ef
Maftuna <98867890+kifo3@users.noreply.github.com>
2023-01-30 18:04:28 +0300

    class methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+            isCounting: false
+        }
+    }
+
+
+
+     componentDidMount() {
+        console.log('componentDidMount')
+        const userCount = localStorage.getItem("timer")
+        if (userCount) {
+            this.setState({count: +userCount})
+        }
+     }
+
+      componentDidUpdate() {
+        console.log('componentDidUpdate')
+        localStorage.setItem("timer", this.state.count)
+     }
+
+
+     handleStart = () => {
+        this.setState({isCounting: true})
+
+        this.countId = setInterval(() => {
+            this.setState({count: this.state.count + 1})
+        }, 1000)
+    }
+
+    handleStop = () => {
+        this.setState({isCounting: false})
+         clearInterval(this.countId)
+    }
+
+    handleReset = () => {
+        this.setState({isCounting:false, count: 0})
+        clearInterval(this.countId)
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <h1>React Timer</h1>
+              <p>{this.state.count}</p>
+              {!this.state.isCounting ? (
+                <button className="succes" onClick={this.handleStart}>
+                    Start
+                </button>
+              ) : (
+                <button className="denger" onClick={this.handleStop}>
+                    Stop
+                </button>
+              )}
+              <button className="reset" onClick={this.handleReset}>
+                    Reset
+                </button>
+            </div>
+    );
+    }
 }
 
 export default App;
